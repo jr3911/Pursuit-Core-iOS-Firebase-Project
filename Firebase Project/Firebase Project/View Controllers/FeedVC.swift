6ce60d9db299381d73182e3b9c41851c0f8f2769
Jason Ruan <jasonruan@pursuit.org>
2019-11-18 15:05:48 -0500

    Initial commit, installs Firebase pods

---
+++ Firebase Project/Firebase Project/View Controllers/FeedVC.swift
@@ -0,0 +1,30 @@
+//
+//  FeedVC.swift
+//  Firebase Project
+//
+//  Created by Jason Ruan on 11/18/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Firebase Project/Firebase Project/Views/FeedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCollectionViewCell.swift
+//  Firebase Project
+//
+//  Created by Jason Ruan on 11/18/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+    
+}
