a31693af5c449a2bd31b9e6295d2d0be7906d84a
Jason Ruan <jasonruan@pursuit.org>
2019-11-27 15:01:51 -0500

    Adds alert controller with textfield to custom extension on UIViewController

--- Firebase Project/Firebase Project/Custom Extensions/ViewControllerExtension.swift
+++ Firebase Project/Firebase Project/Custom Extensions/ViewControllerExtension.swift
@@ -23,4 +23,43 @@ extension UIViewController {
             present(alert, animated: true, completion: nil)
         }
     }
+    
+    public func showAlertWithTextField(title: String, message: String, label: UILabel?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "Enter a display name"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { [weak alert] (_) in
+            guard let textfield = alert?.textFields?[0] else { return }
+            guard textfield.hasText else {
+                self.showAlert(title: "Not a valid display name", message: "Please enter a display name", autoDismiss: true)
+                return
+            }
+            if let newDisplayName = textfield.text {
+                FirebaseAuthService.manager.updateProfile(displayName: newDisplayName, photoURL: nil) { (result) in
+                    switch result{
+                    case .failure(let error):
+                        print(error)
+                        self.showAlert(title: "Error", message: "Could not change display name", autoDismiss: false)
+                    case .success:
+                        FirestoreService.manager.updateCurrentUser(userName: newDisplayName, photoURL: nil) { (result) in
+                            switch result {
+                            case .success:
+                                self.showAlert(title: "All set!", message: "Your display name was updated successfully", autoDismiss: true)
+                                if let label = label {
+                                    label.text = newDisplayName
+                                }
+                            case .failure(let error):
+                                self.showAlert(title: "Error", message: "Could not update display name to Firestore database\nError: \(error.localizedDescription)", autoDismiss: false)
+                            }
+                        }
+                    }
+                }
+            }
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
