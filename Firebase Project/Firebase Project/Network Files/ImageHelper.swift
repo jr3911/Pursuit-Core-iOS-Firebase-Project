6815b1435dc981a57003268ed55630d02519a08b
Jason Ruan <jasonruan@pursuit.org>
2019-11-27 15:05:24 -0500

    Defines helper files

--- Firebase Project/Firebase Project/Network Files/ImageHelper.swift
+++ Firebase Project/Firebase Project/Network Files/ImageHelper.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+import UIKit
+
+class ImageHelper {
+    
+    // MARK: - Static Properties
+    
+    static let shared = ImageHelper()
+    
+    // MARK: - Instance Methods
+    
+    func getImage(url: URL, completionHandler: @escaping (Result<UIImage, AppError>) -> ()) {
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard error == nil else {
+                completionHandler(.failure(.badURL))
+                return
+            }
+            
+            guard let data = data else {
+                completionHandler(.failure(.noDataReceived))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completionHandler(.failure(.notAnImage))
+                return
+            }
+            
+            completionHandler(.success(image))
+            }.resume()
+    }
+    
+    // MARK: - Private Properties and Initializers
+    
+    private init() {}
+}
