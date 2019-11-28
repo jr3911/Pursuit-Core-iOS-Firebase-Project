c1dfef3f9a02c5ac60c43cd1930af04bf0c4a9c0
Jason Ruan <jasonruan@pursuit.org>
2019-11-28 16:35:06 -0500

    Edits ImageHelper to cache images

--- Firebase Project/Firebase Project/Network Files/ImageHelper.swift
+++ Firebase Project/Firebase Project/Network Files/ImageHelper.swift
@@ -16,27 +16,43 @@ class ImageHelper {
     
     static let shared = ImageHelper()
     
+    
+    //MARK: - Caching Properties
+    
+    let imageCache = NSCache<NSString, UIImage>()
+    
     // MARK: - Instance Methods
     
     func getImage(url: URL, completionHandler: @escaping (Result<UIImage, AppError>) -> ()) {
-        URLSession.shared.dataTask(with: url) { (data, _, error) in
-            guard error == nil else {
-                completionHandler(.failure(.badURL))
-                return
-            }
+        
+        //Checks cache to see if image was previously stored, before making network call
+        if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
+            completionHandler(.success(cachedImage))
+        } else {
             
-            guard let data = data else {
-                completionHandler(.failure(.noDataReceived))
-                return
-            }
-            
-            guard let image = UIImage(data: data) else {
-                completionHandler(.failure(.notAnImage))
-                return
-            }
-            
-            completionHandler(.success(image))
+            //Makes network call to attempt to return image via url if available
+            URLSession.shared.dataTask(with: url) { (data, _, error) in
+                guard error == nil else {
+                    completionHandler(.failure(.badURL))
+                    return
+                }
+                
+                guard let data = data else {
+                    completionHandler(.failure(.noDataReceived))
+                    return
+                }
+                
+                guard let image = UIImage(data: data) else {
+                    completionHandler(.failure(.notAnImage))
+                    return
+                }
+                
+                //Cache image for future use and return it
+                self.imageCache.setObject(image, forKey: url.absoluteString as NSString)
+                completionHandler(.success(image))
+                
             }.resume()
+        }
     }
     
     // MARK: - Private Properties and Initializers
