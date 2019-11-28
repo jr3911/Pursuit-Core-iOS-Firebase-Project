11232b6dad129335bc5b3535e9c3e79504592c83
Jason Ruan <jasonruan@pursuit.org>
2019-11-27 19:23:35 -0500

    Defines FeedCollectionViewCell

--- Firebase Project/Firebase Project/Views/FeedCollectionViewCell.swift
+++ Firebase Project/Firebase Project/Views/FeedCollectionViewCell.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class FeedCollectionViewCell: UICollectionViewCell {
+    //MARK: - UI Objects
+    lazy var imageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Private Functions
+    private func addSubviews() {
+        contentView.addSubview(imageView)
+        constrainSubviews()
+    }
+    
+    private func constrainSubviews() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
     
 }
